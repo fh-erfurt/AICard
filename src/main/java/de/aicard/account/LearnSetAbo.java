c37ec4b695d9d5c3a9b32e0966c6af284fbc7ab3
danielmichel@posteo.de <1GguiE,dGdK>
2020-12-19 14:07:30 +0100

    created class LearnSetAbo. Added ToDos to be implemented in other classes for LearnSetAbo to work.

--- src/main/java/de/aicard/account/LearnSetAbo.java
+++ src/main/java/de/aicard/account/LearnSetAbo.java
@@ -1,10 +1,91 @@
 package de.aicard.account;
 
+import de.aicard.Card;
+import de.aicard.card.CardList;
 import de.aicard.card.CardStatus;
+import de.aicard.enums.CardKnowledgeLevel;
+import de.aicard.enums.State;
+import de.aicard.learnset.LearnSet;
+import de.aicard.learnset.LearningSession;
 
 import java.util.ArrayList;
 
 public class LearnSetAbo
 {
     private ArrayList<CardStatus> m_cardStatus;
+    private State m_learnSetStatus;
+    private LearnSet m_learnSet;
+
+    public State getM_learnSetStatus() {
+        return m_learnSetStatus;
+    }
+
+    public void setM_learnSetStatus(State m_learnSetStatus) {
+        this.m_learnSetStatus = m_learnSetStatus;
+    }
+
+    public LearnSet getM_learnSet() {
+        return m_learnSet;
+    }
+
+    public void setM_learnSet(LearnSet m_learnSet) {
+        this.m_learnSet = m_learnSet;
+    }
+
+
+
+    public ArrayList<CardStatus> getM_cardStatus() {
+        return m_cardStatus;
+    }
+
+    public void setM_cardStatus(ArrayList<CardStatus> m_cardStatus) {
+        this.m_cardStatus = m_cardStatus;
+    }
+
+    public LearnSetAbo(LearnSet _learnSet){
+        m_learnSet = _learnSet;
+        m_learnSetStatus = State.NEW;
+        m_cardStatus = new ArrayList<CardStatus>();
+        //TODO Add initialization of cardStatus to constructor, when CardStatus is implemented
+    }
+
+    private CardList getCardOfKnowledgeLevel(CardKnowledgeLevel level){
+        //TODO check when CardStatus is implemented
+        CardList result = new CardList();
+        for(int i=0; i<(this.m_cardStatus.size()); i++){
+            CardStatus status = this.m_cardStatus.get(i);
+            if (status.getCardKnowledgeLevel() == level){
+                result.addToList(status.card);
+            }
+        }
+        return result;
+    }
+
+    private CardList createCardListForSession(int _numOfCards){
+        CardList resultCardList = new CardList();
+
+        for (CardKnowledgeLevel level : CardKnowledgeLevel.values()) {
+            CardList lowestLevelList = getCardOfKnowledgeLevel(level);
+            if(lowestLevelList != null){
+                for(int i = 0; i< lowestLevelList.getListLength(); i++){
+                    while (resultCardList.getListLength()<_numOfCards){
+                        resultCardList.addToList(lowestLevelList.get(i)); //TODO CardList braucht eine allgemeine get Methode für Elemente der Liste
+                    }
+                }
+            }
+            if (resultCardList.getListLength()==_numOfCards) break;
+        }
+
+        return resultCardList;
+    }
+
+    public LearningSession createLearningSession(int _numOfCards){
+
+        CardList sessionList = createCardListForSession(_numOfCards);
+        //TODO LearningSession needs constructor with only a cardList as parameter, because the list can only be created here
+
+        return new LearningSession(sessionList);
+
+
+    }
 }
