7190b6a814fe2be7f5de6e3e9dc2cb73beac4276
riddickse <73823731+riddickse@users.noreply.github.com>
2020-12-23 19:24:36 +0100

    Account modified

--- src/main/java/de/aicard/account/Message.java
+++ src/main/java/de/aicard/account/Message.java
@@ -1,4 +1,21 @@
 package de.aicard.account;
+import de.aicard.enums.Visibility;
+
+import java.time.LocalDateTime;
+
 
 public class Message {
+
+    private String message;
+    private Account sender;
+    private int likes;
+    private LocalDateTime time;
+
+
+    public Message(){
+        message = null;
+        sender = new Account();
+        likes = 0;
+    }
+
 }
