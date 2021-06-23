ff7c7f6fbf865f8a6d11e598591bedf647853673
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-06-23 19:45:02 +0200

    Changed pom.xml; added index.html

--- src/main/java/de/aicard/controller/MainController.java
+++ src/main/java/de/aicard/controller/MainController.java
@@ -1,11 +1,12 @@
 package de.aicard.controller;
-//TODO dependency
+
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
-        //@GetMapping("/")
+    
+        @GetMapping("/")
         public String welcome() {
             return "index";
         }
