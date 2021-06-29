d7b96608d78cbe69d629251b5bf593c72a5b7d27
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-06-29 17:44:44 +0200

    Created CardOverview ControlerMethods
    Co-Authored-By: ClemensESB <73823472+ClemensESB@users.noreply.github.com>

--- src/main/java/de/aicard/controller/LearnSetController.java
+++ src/main/java/de/aicard/controller/LearnSetController.java
@@ -2,6 +2,7 @@ package de.aicard.controller;
 
 import de.aicard.domains.account.Account;
 import de.aicard.domains.account.Student;
+import de.aicard.domains.enums.Visibility;
 import de.aicard.domains.learnset.CardList;
 import de.aicard.domains.learnset.LearnSet;
 import de.aicard.storages.AccountRepository;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -75,15 +77,15 @@ public class LearnSetController
     
             List<LearnSet> learnSetListAdmin = learnSetRepository.findAdminLearnsets(Long.parseLong(accountID));
             List<LearnSet> learnSetListFollowed = learnSetRepository.findFollwedLearnsets(Long.parseLong(accountID));
-            System.out.println(learnSetListAdmin.size());
-            if(learnSetListAdmin.size() >= 1 || learnSetListFollowed.size() >= 1 ){
-                for(LearnSet learnSet : learnSetListAdmin){
-                    System.out.println("Admin: "+learnSet.getTitle());
-                }
-                for(LearnSet learnSet : learnSetListFollowed){
-                    System.out.println("Followed: "+learnSet.getTitle());
-                }
-            }
+//            System.out.println(learnSetListAdmin.size());
+//            if(learnSetListAdmin.size() >= 1 || learnSetListFollowed.size() >= 1 ){
+//                for(LearnSet learnSet : learnSetListAdmin){
+//                    System.out.println("Admin: "+learnSet.getTitle());
+//                }
+//                for(LearnSet learnSet : learnSetListFollowed){
+//                    System.out.println("Followed: "+learnSet.getTitle());
+//                }
+//            }
             model.addAttribute("learnSetListAdmin", learnSetListAdmin);
             model.addAttribute("learnSetListFollowed", learnSetListFollowed);
         }
@@ -93,9 +95,32 @@ public class LearnSetController
 
     
     
-    @GetMapping("cardOverview")
-    public String getCardOverview(Model model)
+    @GetMapping("cardOverview/{id}")
+    public String getCardOverview(@PathVariable Long id,HttpServletRequest request,HttpServletResponse response, Model model)
     {
-        return "cardOverview";
+        Optional<LearnSet> learnSet =  learnSetRepository.findById(id);
+        String cookieValue = getCookieContent(request.getCookies(), "javaSession");
+        List<Account> accounts = accountRepository.findAllById(Long.parseLong(cookieValue));
+        
+        if(learnSet.get().getVisibility() == Visibility.PUBLIC ||
+                learnSet.get().getVisibility() == Visibility.PRIVATE &&
+                        learnSet.get().getAdminList().contains(accounts.get(0)) ||
+                            learnSet.get().getVisibility() == Visibility.PROTECTED &&
+                                    accounts.get(0).getFaculty() == learnSet.get().getFaculty())
+        {
+            model.addAttribute("learnSet", learnSetRepository.findById(id));
+            System.out.println(learnSetRepository.findById(id).get().getTitle());
+            
+            return "cardOverview";
+        }
+        else
+        {
+            return "redirect:/index";
+        }
+        
     }
+    
+
+    
+    
 }
