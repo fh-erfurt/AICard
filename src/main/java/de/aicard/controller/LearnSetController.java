6ba0f4606522cadc87a0e7d37f51951ea3a5c912
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-06-29 11:46:45 +0200

    create LearnSet works
    takes User input and userID from CookieSession to create and save a new LearnSet to the Database

--- src/main/java/de/aicard/controller/LearnSetController.java
+++ src/main/java/de/aicard/controller/LearnSetController.java
@@ -1,9 +1,12 @@
 package de.aicard.controller;
 
 import de.aicard.domains.account.Account;
+import de.aicard.domains.account.Student;
+import de.aicard.domains.learnset.CardList;
 import de.aicard.domains.learnset.LearnSet;
 import de.aicard.storages.AccountRepository;
 import de.aicard.storages.LearnSetRepository;
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static de.aicard.config.Session.getCookieContent;
 
@@ -25,6 +32,7 @@ public class LearnSetController
     @Autowired
     AccountRepository accountRepository;
     
+    
     @GetMapping("/createLearnset")
     public String signUp(Model model)
     {
@@ -36,9 +44,23 @@ public class LearnSetController
     }
     
     @PostMapping("/createLearnset")
-    public String createLearnset(@ModelAttribute("newLearnset") LearnSet newLearnset, Model model/*, HttpServletRequest request, HttpServletResponse response*/)
+    public String createLearnset(@ModelAttribute("newLearnset") LearnSet newLearnset, Model model, HttpServletRequest request, HttpServletResponse response)
     {
         //TODO: get id from Cookie, save Learnset with user as Owner and Owner as first person in adminList
+        String accountID = getCookieContent(request.getCookies(), "javaSession");
+        List<Account> accountList = accountRepository.findAllById(Long.parseLong(accountID));
+        if(accountList.size() == 1)
+        {
+            System.out.println(accountList.get(0).toString());
+            newLearnset.setOwner(accountList.get(0));
+            newLearnset.setAdminList(new ArrayList<>());
+            newLearnset.addAdmin(accountList.get(0));
+            newLearnset.setCardList(new CardList());
+            newLearnset.setCommentList(new ArrayList<>());
+    
+            learnSetRepository.save(newLearnset);
+        }
+        
         return "index";
     }
     
