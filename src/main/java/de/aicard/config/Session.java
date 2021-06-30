a5aa322001dbf7c61181f74a1612452ee5d52105
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-06-30 17:05:53 +0200

    started JSON Session

--- src/main/java/de/aicard/config/Session.java
+++ src/main/java/de/aicard/config/Session.java
@@ -1,10 +1,27 @@
 package de.aicard.config;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.aicard.domains.Social.Chat;
+import de.aicard.domains.account.Account;
+import de.aicard.domains.account.Professor;
+import de.aicard.domains.card.Card;
+import de.aicard.domains.card.CardContent;
+import de.aicard.domains.card.TextFile;
+import de.aicard.domains.enums.AcademicGrade;
+import de.aicard.domains.enums.Faculty;
+import de.aicard.domains.learnset.LearnSet;
+import de.aicard.domains.learnset.LearnSetAbo;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 
 import javax.servlet.http.Cookie;
+import java.util.ArrayList;
 
 @Setter
 @Getter
@@ -37,17 +54,55 @@ public class Session {
         return null;
     }
     
+//  /*  public static Cookie addSessionValue(Cookie cookies[], String jsonKey, String jsonValue)
+//    {
+//
+//    */}
+//
+    
+//    public static String getSessionValueByKey(Cookie cookies[], String jsonKey)
+//    {
+//
+//        if (cookies != null)
+//            for (Cookie cookie : cookies)
+//            {
+//                if (cookie.getName().equals(cookieName))
+//                {
+////                    System.out.println(cookie.getValue());
+//
+//                    String cookieValue = cookie.getValue();
+////                    String cookieValue = "{\"name\":\"derNameZumKey\"}";
+//                    JsonObject jsonObject = new JsonParser().parse(cookieValue).getAsJsonObject();
+//                    String test = jsonObject.get(jsonKey).toString();
+//                    System.out.println(test);
+//                    return jsonObject.get(jsonKey).toString();
+////                    Gson gson = new Gson();
+////
+////
+////                    String json = gson.toJson(new Card((CardContent) new TextFile("FrontSide"), (CardContent) new TextFile("BackSide")));
+////                    System.out.println(json);
+////
+////                    JsonElement jsonElement = gson.toJsonTree(new Card((CardContent) new TextFile("FrontSide"), (CardContent) new TextFile("BackSide")));
+////                    jsonElement.getAsJsonObject().addProperty("testProperty", "testValue");
+////                    String newJson = gson.toJson(jsonElement);
+////                    System.out.println(newJson);
+////
+////                    JsonElement jsonElement1 = gson.toJsonTree(new Object());
+////                    System.out.println(" Test -> "  + jsonElement1);
+////
+////                    jsonElement1.getAsJsonObject().addProperty("key", "value");
+////                    System.out.println("TEst 2 -> " + jsonElement1);
+////                    return "";
+//                }
+//            }
+//        return null;
+//    }
+    
     public static String getCookieContent(Cookie cookies[])
     {
-        if (cookies != null)
-            for (Cookie cookie : cookies)
-            {
-                if (cookie.getName().equals(cookieName))
-                {
-//                    System.out.println(cookie.getValue());
-                    return cookie.getValue();
-                }
-            }
-        return "No Cookies found";
+        return "1";
     }
+    
+    
+    
 }
