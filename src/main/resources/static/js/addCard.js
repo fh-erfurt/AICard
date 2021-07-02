f9425320d87628089f71307483c374805c0ccd52
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-07-02 14:24:49 +0200

    prepared static addCard files for addCardController

--- src/main/resources/static/js/addCard.js
+++ src/main/resources/static/js/addCard.js
@@ -1,47 +1,92 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     const cardDiv = document.getElementById("addCard");
-//     const btn = document.getElementById("add");
-//
-//     const kase = document.getElementById("kase");
-//
-//     btn.addEventListener("click",()=>{
-//         var addCard = document.createElement("div");
-//         addCard.classList.add("card");
-//
-//         var form = document.createElement("form");
-//         form.method = "post";
-//         form.action = "/addCard";
-//
-//         var input1 = document.createElement("input");
-//         input1.classList.add("txt-input");
-//         input1.name ="front";
-//         input1.type = "text";
-//         form.appendChild(input1);
-//
-//         var input2 = document.createElement("input");
-//         input2.classList.add("txt-input");
-//         input2.name ="back";
-//         input2.type = "text";
-//         form.appendChild(input2);
-//
-//         var save = document.createElement("button");
-//         save.classList.add("btn");
-//         save.classList.add("save");
-//         save.id = "saveCard";
-//         save.innerHTML = "save";
-//         save.name = "save";
-//         form.appendChild(save);
-//
-//         var del = document.createElement("button");
-//         del.classList.add("btn")
-//         del.classList.add("del");
-//         del.id = "deleteCard";
-//         del.innerHTML = "X";
-//         del.name = "delete";
-//         form.appendChild(del);
-//
-//         addCard.appendChild(form);
-//         cardDiv.appendChild(addCard);
-//     });
-// });
-//
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    var cardFrontType = document.getElementById("cardFrontType");
+    var cardBackType = document.getElementById("cardBackType");
+
+
+    var textFileFrontInput = document.getElementById("textFileFrontInput");
+    var pictureFileFrontInput = document.getElementById("pictureFileFrontInput");
+    var videoFileFrontInput = document.getElementById("videoFileFrontInput");
+    var audioFileFrontInput = document.getElementById("audioFileFrontInput");
+
+
+    var textFileBackInput = document.getElementById("textFileBackInput");
+    var pictureFileBackInput = document.getElementById("pictureFileBackInput");
+    var videoFileBackInput = document.getElementById("videoFileBackInput");
+    var audioFileBackInput = document.getElementById("audioFileBackInput");
+
+    cardFrontType.addEventListener("change", function ()
+    {
+        if(cardFrontType.value === "TextFile") console.log("TIE");
+
+        if(cardFrontType.value === "TextFile")
+        {
+            console.log(cardFrontType.value);
+            textFileFrontInput.style.display = "block";
+            pictureFileFrontInput.style.display = "none";
+            videoFileFrontInput.style.display = "none";
+            audioFileFrontInput.style.display = "none";
+        }
+        if(cardFrontType.value === "AudioFile")
+        {
+            console.log(cardFrontType.value);
+            textFileFrontInput.style.display = "none";
+            pictureFileFrontInput.style.display = "none";
+            videoFileFrontInput.style.display = "none";
+            audioFileFrontInput.style.display = "block";
+        }
+        if(cardFrontType.value === "PictureFile")
+        {
+            console.log(cardFrontType.value);
+            textFileFrontInput.style.display = "none";
+            pictureFileFrontInput.style.display = "block";
+            videoFileFrontInput.style.display = "none";
+            audioFileFrontInput.style.display = "none";
+        }
+        if(cardFrontType.value === "VideoFile")
+        {
+            console.log(cardFrontType.value);
+            textFileFrontInput.style.display = "none";
+            pictureFileFrontInput.style.display = "none";
+            videoFileFrontInput.style.display = "block";
+            audioFileFrontInput.style.display = "none";
+        }
+
+
+    });
+
+    cardBackType.addEventListener("change", function ()
+    {
+
+        if(cardBackType.value === "TextFile")
+        {
+            textFileBackInput.style.display = "block";
+            pictureFileBackInput.style.display = "none";
+            videoFileBackInput.style.display = "none";
+            audioFileBackInput.style.display = "none";
+        }
+        if(cardBackType.value === "PictureFile")
+        {
+            textFileBackInput.style.display = "none";
+            pictureFileBackInput.style.display = "block";
+            videoFileBackInput.style.display = "none";
+            audioFileBackInput.style.display = "none";
+        }
+        if(cardBackType.value === "VideoFile")
+        {
+            textFileBackInput.style.display = "none";
+            pictureFileBackInput.style.display = "none";
+            videoFileBackInput.style.display = "block";
+            audioFileBackInput.style.display = "none";
+        }
+        if(cardBackType.value === "AudioFile")
+        {
+            textFileBackInput.style.display = "none";
+            pictureFileBackInput.style.display = "none";
+            videoFileBackInput.style.display = "none";
+            audioFileBackInput.style.display = "block";
+        }
+    });
+
+
+});
