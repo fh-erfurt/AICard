a6e62e0de4f422be7b26791d98f04f8564ebe07f
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-07-02 15:01:40 +0200

    Blueprint and Test for cardFile uploading for addCard

--- src/main/java/de/aicard/controller/MainController.java
+++ src/main/java/de/aicard/controller/MainController.java
@@ -1,6 +1,7 @@
 package de.aicard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -15,5 +16,11 @@ public class MainController {
         public String notWelcome(){
             return "index";
         }
-
+    
+    @GetMapping("/saveFile")
+    public String getSaveFile(Model model)
+    {
+        
+        return "saveFile";
+    }
 }
