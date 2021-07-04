ea3fe6f823eac1778d4ade9224fc2e6649915c60
Amine Semlali <amine.semlali@fh-erfurt.de>
2021-07-04 16:58:44 +0200

    Account Controller but still lot to do

---
+++ src/main/java/de/aicard/controller/AccountController.java
@@ -0,0 +1,92 @@
+package de.aicard.controller;
+
+import de.aicard.config.Session;
+import de.aicard.domains.account.Account;
+import de.aicard.domains.account.Professor;
+import de.aicard.domains.account.Student;
+import de.aicard.storages.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+
+
+public class AccountController {
+
+
+    private static final String patternReg = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @GetMapping("/profile")
+    public String showProfile(Model model,HttpServletRequest request) {
+        List<String> errors = new ArrayList<>();
+        String cookieValue = Session.getSessionValue(request.getCookies());
+        if(cookieValue == null){
+            return "redirect:/login";
+        }
+        return "profile";
+    }
+    @RequestMapping("/editAccount")
+        public Model getEditAccount(HttpServletRequest request,Model model) {
+
+        String accountID = Session.getSessionValue(request.getCookies());
+        Optional<Account> account = accountRepository.findById(Long.parseLong(accountID));
+        model.addAttribute("account", account);
+
+        return model;
+    }
+    @PostMapping("/updateAccount")
+    public String editAccount(@ModelAttribute("account") Account theAccount, Model model) throws NoSuchAlgorithmException {
+
+        List<String> errors = new ArrayList<>();
+        Optional<Account> oldAccount = accountRepository.findById(theAccount.getId());
+        String accountEmail = theAccount.getEmail();
+        Optional<Account> matchingEntries = accountRepository.findByEmail(accountEmail);
+        if (matchingEntries.isPresent() && matchingEntries.getId() != oldAccount.getId()) {
+
+            errors.add("Ein Account mit diser E-Mail Adresse existiert bereits");
+            System.out.println("Ein Account mit diser E-Mail Adresse existiert bereits");
+            model.addAttribute("errorList",errors);
+            return "editAccount";
+        }
+        oldAccount.setEmail(accountEmail);
+
+
+        Pattern pattern = Pattern.compile(patternReg);
+        String password = theAccount.getPassword();
+        Matcher matcher = pattern.matcher(password);
+        //creating md5 hash
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String hashedPassword = DatatypeConverter.printHexBinary(digest);
+
+        if (!matcher.matches()) {
+            errors.add("Passwort entspricht nicht den Passwortrichtlinien");
+            System.out.println("password not matched");
+            model.addAttribute("errorList",errors);
+            return "editAccount";
+        }
+        oldAccount.setPassword(hashedPassword);
+        oldAccount.setName(theAccount.getName());
+        oldAccount.setDescription(theAccount.getDescription());
+        oldAccount.setFaculty(theAccount.getFaculty());
+
+        accountRepository.save(oldAccount);
+        return "profile";
+    }
+
+
+}
--- src/main/java/de/aicard/storages/AccountRepository.java
+++ src/main/java/de/aicard/storages/AccountRepository.java
@@ -24,6 +24,8 @@ public interface AccountRepository extends JpaRepository<Account, Long>
     Professor findProfessorById(Long id);
     Optional<Account> findByName(String name);
     Optional<Account> findByEmail(String email);
+
+
     
     // is used to determain what if Account is Student or Professor
     List<Account> findAllById(Long id);
