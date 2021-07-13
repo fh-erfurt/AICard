6ca8c2df5cc05f336f6b8253ea2cd75fa2180dbb
ClemensESB <clm.st.berger@gmail.com>
2021-07-13 21:48:37 +0200

    password and email matcher

---
+++ src/test/java/de/aicard/rand/patternTest.java
@@ -0,0 +1,27 @@
+package de.aicard.rand;
+
+import de.aicard.config.RegPattern;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+public class patternTest {
+
+    @Test
+    public void passTest(){
+        String passwordTrue = "aaBB123##";
+        Assertions.assertTrue(RegPattern.passMatches(passwordTrue));
+        String passwordFalse = "a1#";
+        Assertions.assertFalse(RegPattern.passMatches(passwordFalse));
+    }
+
+    @Test
+    public void emailTest(){
+        String emailTrue = "wa1d@kalt.de";
+        Assertions.assertTrue(RegPattern.emailMatches(emailTrue));
+        String emailFalse = "waid@K#alt.de";
+        Assertions.assertFalse(RegPattern.emailMatches(emailFalse));
+
+    }
+
+}
