b202b3c871646ea941b47bf8924878cb12122074
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-08-11 16:27:25 +0200

    added filter for Faculty and Name to learnSetShop page
    Co-Authored-By: ClemensESB <73823472+ClemensESB@users.noreply.github.com>

--- src/main/java/de/aicard/controller/LearnSetShopController.java
+++ src/main/java/de/aicard/controller/LearnSetShopController.java
@@ -1,6 +1,7 @@
 package de.aicard.controller;
 
 import de.aicard.domains.account.Account;
+import de.aicard.domains.enums.Faculty;
 import de.aicard.domains.learnset.LearnSet;
 import de.aicard.domains.learnset.LearnSetAbo;
 import de.aicard.storages.AccountRepository;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @Controller
@@ -27,31 +30,74 @@ public class LearnSetShopController
     
     
     @GetMapping("/learnSetShop")
-    public String getLearnSetShop(Principal principal, Model model)
+    public String getLearnSetShop(@RequestParam(name = "faculty", required = false, defaultValue = "duNull")String stringFaculty,
+                                  @RequestParam(name="learnSetTitle", required = false, defaultValue = "") String learnSetTitle,
+                                  Principal principal, Model model)
     {
         List<LearnSet> learnSets = learnSetRepository.findAll();
         List<LearnSet> frontEndLearnSets = new ArrayList<>();
         Optional<Account> account = accountRepository.findByEmail(principal.getName());
         if(!learnSets.isEmpty() && account.isPresent())
         {
-            for (LearnSet learnset:learnSets)
+            // check if Account can Access these LearnSets and if the Account follows already
+            for (LearnSet learnset : learnSets)
             {
-                if(learnset.isAuthorizedToAccessLearnSet(account.get())){
+                if (learnset.isAuthorizedToAccessLearnSet(account.get()))
+                {
                     boolean inList = false;
                     for (LearnSetAbo learnSetAbo : account.get().getLearnsetAbos())
                     {
-                        if(learnSetAbo.getLearnSet().equals(learnset) && !inList)
+                        if (learnSetAbo.getLearnSet().equals(learnset) && ! inList)
                         {
                             inList = true;
                         }
                     }
-                    if(!inList)
+                    if (! inList)
                     {
                         frontEndLearnSets.add(learnset);
                     }
-                    
+    
                 }
             }
+    
+            // evaluate RequestParams
+    
+            // if we want to filter for faculty -> filter!
+            System.out.println("faculty:" + stringFaculty);
+            List<LearnSet> helperLearnSets = new ArrayList<>();
+            try
+            {
+                Faculty.valueOf(stringFaculty);
+    
+                for (LearnSet learnSet : frontEndLearnSets)
+                {
+                    if (learnSet.getFaculty().equals(Faculty.valueOf(stringFaculty)))
+                    {
+                        helperLearnSets.add(learnSet);
+                    }
+                }
+                frontEndLearnSets = helperLearnSets;
+                helperLearnSets = new ArrayList<>();
+    
+            } catch (IllegalArgumentException e)
+            {
+                System.out.println(e);
+            }
+    
+            // filter for learnset->title containt substring learnSetTitle
+            if(!learnSetTitle.isEmpty())
+            {
+                for (LearnSet learnSet : frontEndLearnSets)
+                {
+                    if (learnSet.getTitle().toLowerCase().contains(learnSetTitle.toLowerCase()))
+                    {
+                        helperLearnSets.add(learnSet);
+                    }
+                }
+                frontEndLearnSets = helperLearnSets;
+                helperLearnSets = new ArrayList<>();
+            }
+            
             model.addAttribute("learnSetList",frontEndLearnSets);
         }
         return"/learnSetShop";
